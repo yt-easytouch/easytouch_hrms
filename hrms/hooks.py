8bc48fc2d79f21b3adfad2d0d4b37c6d0f8448d3
Abdisalam Nor <me@engacs.pw>
2024-08-01 18:09:21 +0300

    Update hooks.py

--- hrms/hooks.py
+++ hrms/hooks.py
@@ -4,7 +4,7 @@ app_publisher = "Frappe Technologies Pvt. Ltd."
 app_description = "Modern HR and Payroll Software"
 app_email = "contact@frappe.io"
 app_license = "GNU General Public License (v3)"
-required_apps = ["frappe/erpnext"]
+required_apps = ["yt-easytouch/erpnext"]
 source_link = "http://github.com/frappe/hrms"
 
 
